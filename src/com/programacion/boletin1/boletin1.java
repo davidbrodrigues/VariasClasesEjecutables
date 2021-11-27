9ce58d8cdc65fae78f1a94542c696e03c3b88b1d
David Braga Rodrígues <dbragarodrigues@danielcastelao.com>
2021-11-27 15:59:35 +0100

    Hemos creado dos paquetes.

---
+++ src/com/programacion/boletin1/boletin1.java
@@ -0,0 +1,5 @@
+package com.programacion.boletin1;
+
+public class boletin1 {
+    //Hola Ej 1 del Boletin 1
+}
---
+++ src/com/programacion/boletin2/boletin2.java
@@ -0,0 +1,5 @@
+package com.programacion.boletin2;
+
+public class boletin2 {
+    //Hola Ej 1 del Boletin 2
+}
